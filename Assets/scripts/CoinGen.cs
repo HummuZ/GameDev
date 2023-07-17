7a2796862a9fbd0ba8ba30e43346631be587aff7
Hummus <moqdam13@gmail.com>
2023-07-17 19:57:13 +0400

    finished

--- Assets/scripts/CoinGen.cs
+++ Assets/scripts/CoinGen.cs
@@ -7,16 +7,19 @@ public class CoinGen : MonoBehaviour
 {
     public GameObject Coin;
     int count = 0;
+    GameObject[] CoinArr = new GameObject[20];
     // Start is called before the first frame update
     void Start()
     {
         
         while (count < 5)
         {
-            GameObject c = Instantiate(Coin) as GameObject;
-            c.transform.position = new Vector3(Random.Range(-3.3f, 3.8f), 0.5f, Random.Range(-3.1f, 3.4f));
+            CoinArr[count] = Instantiate(Coin) as GameObject;
+            CoinArr[count].transform.position = new Vector3(Random.Range(-3.3f, 3.8f), 0.5f, Random.Range(-3.1f, 3.4f));
             count++;
         }
+
+
     }
 
     // Update is called once per frame
